ad1846b231b7365179611db7eb3480cf7d8a38b4
Nirupon <niruponpal@gmail.com>
2025-11-22 23:56:42 +0530

    models updated

--- Storefront/playground/models.py
+++ Storefront/playground/models.py
@@ -4,3 +4,12 @@ class Products(models.Models):
     title=models.CharField(max_length=100)
     description=models.TextField()
     price=models.DecimalField()
+    Inventory=models.IntegerField()
+    last_update=models.DateTimeField(auto_now=True)
+
+class Customer(models.Models):
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+    email=models.EmailField(unique=True)
+    phone=models.CharField(max_length=255)
+    birth_date=models.DateField(null=True)
