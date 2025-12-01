633c5844b8bb63f8070e96464f5c690077a3b326
Nirupon <niruponpal@gmail.com>
2025-12-02 00:38:12 +0530

    O-objects and F-Objects learned

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from django.db.models import Q,F
 from store.models import Product
 
 def say_hello(request):
-    query_set = Product.objects.filter(unit_price__range=(20, 30))
+    query_set = Product.objects.filter(Q(unit_price__gt=20) & Q(title__icontains='coffee'))
+   # query_set = Product.objects.filter(unit_price__gt=F('collection__id') * 10)
     
     return render(request, 'index.html', {'products': list(query_set)})
