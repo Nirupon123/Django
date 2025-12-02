63493d367936df3d41fa4f5304f990149c247a0d
Nirupon <niruponpal@gmail.com>
2025-12-02 23:51:02 +0530

    query set updated

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -5,5 +5,5 @@ from store.models import Product
 
 def say_hello(request):
     query_set = Product.objects.filter(Q(unit_price__gt=20) & Q(title__icontains='coffee'))
-   # query_set = Product.objects.filter(unit_price__gt=F('collection__id') * 10)
+    query_set = Product.objects.filter(unit_price__gt=F('collection__id') * 10)
     return render(request, 'index.html', {'products': list(query_set)})
