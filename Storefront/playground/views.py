fc90a6771e5fe2642d666ae76daecde18e234cff
Nirupon <niruponpal@gmail.com>
2025-12-03 21:10:26 +0530

    Removed sensitive files and updated .gitignore

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
 from django.db.models import Q,F
-from store.models import Product
+from store.models import Product,OrderItem
+from store.models import Collection
 
 def say_hello(request):
-    query_set = Product.objects.filter(Q(unit_price__gt=20) & Q(title__icontains='coffee'))
-    query_set = Product.objects.filter(unit_price__gt=F('collection__id') * 10)
-    return render(request, 'index.html', {'products': list(query_set)})
+    #query_set = Product.objects.filter(Q(unit_price__gt=20) & Q(title__icontains='coffee'))
+    #query_set = Product.objects.filter(unit_price__gt=F('collection__id') * 10)
+    #query_set=OrderItem.objects.values('product_id').distinct()
+    #tagedItems.objects.get_by_model(Product,1)
+    collection = Collection()
+    collection.title = "test_input"
+    collection.featured_product = Product(pk=1)
+    collection.save()
+
+    return render(request, 'index.html')
--- Storefront/tags/models.py
+++ Storefront/tags/models.py
@@ -2,11 +2,20 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
-
+class taggedItemManager(models.Manager):
+    def get_by_model(self,obj_type,obj_id):
+        content_type=ContentType.objects.get_for_model(obj_type)
+        return tagedItem.objects. \
+        select_related('tag')\
+        .filter( 
+            content_type=content_type,
+            object_id=obj_id)
+    
 class tag(models.Model):
     label=models.CharField(max_length=50)
 
 class tagedItem(models.Model):
+    objects=taggedItemManager()
     tag=models.ForeignKey(tag,on_delete=models.CASCADE)
     content_type=models.ForeignKey(ContentType,on_delete=models.CASCADE)
     object_id=models.PositiveIntegerField()
