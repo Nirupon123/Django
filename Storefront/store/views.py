913688b10a0a278d8fe634a5bcff3cd8923cc40d
Nirupon <niruponpal@gmail.com>
2025-12-06 03:08:59 +0530

    views updated

--- Storefront/store/views.py
+++ Storefront/store/views.py
@@ -3,13 +3,23 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from .serializer import ProductSerializer
+from rest_framework import status
 
 
-@api_view()
+@api_view(['GET','POST'])
 def product_list(request):
-    product_set = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(product_set, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        product_set = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(product_set, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.validated_data
+            return Response('OK')
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 
 @api_view()
 def product_detail(request,id):
